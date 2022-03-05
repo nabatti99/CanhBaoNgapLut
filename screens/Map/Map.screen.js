db18a536c265412081aca495cb483c0d831fd1a8
Nguyễn Lê Anh Minh <anhminh2122000@gmai.com>
2022-03-05 17:28:32 +0700

    import Google Map

--- screens/Map/Map.screen.js
+++ screens/Map/Map.screen.js
@@ -1,8 +1,58 @@
 import React from "react";
-import { Text } from "react-native-ui-lib";
+import MapView from "react-native-maps";
+import { Text, View } from "react-native-ui-lib";
 
 function MapScreen() {
-  return <Text>Map</Text>;
+  const customMapStyle = [
+    {
+      featureType: "landscape.natural",
+      elementType: "geometry",
+      stylers: [
+        {
+          color: "#dbeaff",
+        },
+      ],
+    },
+    {
+      featureType: "road",
+      elementType: "geometry",
+      stylers: [
+        {
+          color: "#ffffff",
+        },
+      ],
+    },
+    {
+      featureType: "water",
+      elementType: "geometry",
+      stylers: [
+        {
+          color: "#b7cdf0",
+        },
+      ],
+    },
+  ];
+
+  return (
+    <View flex>
+      <View absF marginB-152 bg-blue300>
+        <MapView
+          style={{
+            flex: 1,
+          }}
+          initialRegion={{
+            latitude: 15.5,
+            longitude: 108.1,
+            latitudeDelta: 1,
+            longitudeDelta: 1,
+          }}
+          showsUserLocation={true}
+          showsCompass={true}
+          customMapStyle={customMapStyle}
+        />
+      </View>
+    </View>
+  );
 }
 
 export default MapScreen;
